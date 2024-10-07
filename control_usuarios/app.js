341472128f5df65bd48542c75985243232e08d52
Vicente Agustin Mayra Yuridia <mayuagustin013@gmail.com>
2024-10-07 12:08:39 -0600

    Express Server

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/usuarios", (req, res) => {
     const usuarios = [
         {
             id: 1,
@@ -16,10 +15,20 @@ app.get("/usuarios", (req, res) => {
             apellido: "Lopez",
             email: "mL0t3@example.com",
         }
-    ]
-    res.status(200).send(usuarios);        
-});
+    ];
 
+    app.get("/usuarios", (req, res) => {
+        res.status(200).send(usuarios);
+    });      
+
+app.get("/usuarios/:id", (req, res) => {
+    const params = req.params;
+    const { id } = params;
+    const usuario = usuarios.find((usuarios) => usuarios.id == id);
+    
+    res.status(200).send("usuario");
+    
+})
 app.listen(3000, () => {
     console.log('Servidor corriendo en el http://localhost:3000');
 });
